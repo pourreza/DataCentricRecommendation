32bdbdc4a4ea894b466a96d1e52d27e8689c96b1
Maryam Pourreza <maryampourreza@Maryams-MacBook-Pro-2.local>
2018-07-19 00:05:22 -0700

    still WIP

--- src/main/java/DataCentricRecommendation/DataCentricRecommendation.java
+++ src/main/java/DataCentricRecommendation/DataCentricRecommendation.java
@@ -1,88 +1,138 @@
 package DataCentricRecommendation;
 
-import org._3pq.jgrapht.edge.DirectedWeightedEdge;
 import org.apache.commons.math3.util.Pair;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.AllDirectedPaths;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
 import serviceWorkflowNetwork.OOperation;
 import serviceWorkflowNetwork.SService;
 import serviceWorkflowNetwork.WorkflowVersion;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
+import static utilities.Printer.print;
 import static utilities.PythonInterpreter2.getCosineSimilarity;
 
 public class DataCentricRecommendation {
 
-    private final Graph<String, DefaultWeightedEdge> graph;
+    private final Graph<String, DefaultWeightedEdge> weightedGraph;
+    private Graph<String, DefaultEdge> graph;
     private ArrayList<Pair<String, String>> allReverseEdges;
     private Set<WorkflowVersion> workflows;
     private Map<String, SService> serviceMap;
+    private Map<String, String> servicesContexts;
 
     public DataCentricRecommendation(Graph<String, DefaultWeightedEdge> completeGraph, Set<WorkflowVersion> workflowVersions, Map<String, SService> serviceMap) {
-        this.graph = completeGraph;
+        this.weightedGraph = completeGraph;
         this.workflows = workflowVersions;
         this.serviceMap = serviceMap;
+        this.servicesContexts = new HashMap<String, String>();
+        createSimpleGraph(weightedGraph);
+    }
+
+    private void createSimpleGraph(Graph<String, DefaultWeightedEdge> weightedGraph) {
+        graph = new SimpleDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+        for(String vertex: weightedGraph.vertexSet()){
+            graph.addVertex(vertex);
+        }
+        for(DefaultWeightedEdge weightedEdge: weightedGraph.edgeSet()){
+            graph.addEdge(weightedGraph.getEdgeSource(weightedEdge), weightedGraph.getEdgeTarget(weightedEdge));
+        }
     }
 
     public double getScore(SService source, SService target) {
-        AllDirectedPaths<String, DefaultWeightedEdge> allDirectedPaths = new AllDirectedPaths<String, DefaultWeightedEdge>(graph);
-        List<GraphPath<String, DefaultWeightedEdge>> allPaths = allDirectedPaths.getAllPaths(source.getURL(), target.getURL(), false, graph.vertexSet().size());
-        double maxScore = 0;
-        for(GraphPath<String, DefaultWeightedEdge> path: allPaths){
-            Graph<String, DefaultWeightedEdge> pathGraph = path.getGraph();
+        DijkstraShortestPath<String, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<String, DefaultEdge>(graph);
+        GraphPath<String, DefaultEdge> path = dijkstraAlg.getPaths(source.getURL()).getPath(target.getURL());
+//        AllDirectedPaths<String, DefaultWeightedEdge> allDirectedPaths = new AllDirectedPaths<String, DefaultWeightedEdge>(weightedGraph);
+//        List<GraphPath<String, DefaultWeightedEdge>> allPaths = allDirectedPaths.getAllPaths(source.getURL(), target.getURL(), false, weightedGraph.vertexSet().size());
+//        double maxScore = 0;
+//        for(GraphPath<String, DefaultWeightedEdge> path: allPaths){
+        if(path!=null) {
+            Graph<String, DefaultEdge> pathGraph = path.getGraph();
             double pathScore = 0;
             int pathLength = pathGraph.edgeSet().size();
-            for(DefaultWeightedEdge pathEdge: pathGraph.edgeSet()){
+            String twoAwaySource = "";
+            String twoAwayTarget = "";
+            int twoAway = 0;
+            int countLoops = 0;
+            for (DefaultEdge pathEdge : pathGraph.edgeSet()) {
                 String edgeSource = pathGraph.getEdgeSource(pathEdge);
                 String edgeTarget = pathGraph.getEdgeTarget(pathEdge);
-                double edgeWeight = graph.getEdgeWeight(pathEdge);
-                pathScore += (intentSimilarity(edgeSource, edgeTarget)*contextSimilarity(edgeSource, edgeTarget)* edgeWeight);
+                if (twoAway % 2 == 0) {
+                    if (edgeSource.equals(twoAwaySource) && edgeTarget.equals(twoAwayTarget)) {
+                        countLoops++;
+                    }
+                }
+                print("edge: "+ twoAway);
+                twoAway++;
+                twoAwaySource = edgeSource;
+                twoAwayTarget = edgeTarget;
+                double edgeWeight = weightedGraph.getEdgeWeight(weightedGraph.getEdge(edgeSource, edgeTarget));
+                double intentSimilarity = intentSimilarity(edgeSource, edgeTarget);
+                double contextSimilarity = contextSimilarity(edgeSource, edgeTarget);
+                pathScore += (intentSimilarity * contextSimilarity * edgeWeight);
             }
             pathScore /= pathLength;
-            if(pathScore>maxScore) {
-                maxScore = pathScore;
-            }
+//            if(pathScore>maxScore) {
+//                maxScore = pathScore;
+//            }
+//        }
+            return pathScore;
         }
-        return maxScore;
+        return 0;
     }
 
     private double contextSimilarity(String service1, String service2) {
         String serviceContext1 = "";
         String serviceContext2 = "";
-        for (WorkflowVersion workflow : workflows) {
-            ArrayList<OOperation> operations = workflow.getExternalOperations();
-            for (OOperation operation : operations) {
-                if (operation.getService().getURL().equals(service1)) {
-                    serviceContext1 += workflow.getIntent()+" ";
-                    break;
+        if(servicesContexts.containsKey(service1)){
+            serviceContext1 = servicesContexts.get(service1);
+        }else{
+            for (WorkflowVersion workflow : workflows) {
+                ArrayList<OOperation> operations = workflow.getExternalOperations();
+                for (OOperation operation : operations) {
+                    if (operation.getService().getURL().equals(service1)) {
+                        serviceContext1 += workflow.getIntent()+" ";
+                        break;
+                    }
                 }
-                if (operation.getService().getURL().equals(service2)) {
-                    serviceContext2 += workflow.getIntent()+" ";
-                    break;
+            }
+            servicesContexts.put(service1, serviceContext1);
+        }
+        if(servicesContexts.containsKey(service2)){
+            serviceContext2 = servicesContexts.get(service2);
+        }else{
+            for (WorkflowVersion workflow : workflows) {
+                ArrayList<OOperation> operations = workflow.getExternalOperations();
+                for (OOperation operation : operations) {
+                    if (operation.getService().getURL().equals(service2)) {
+                        serviceContext2 += workflow.getIntent()+" ";
+                        break;
+                    }
                 }
+                servicesContexts.put(service2, serviceContext2);
             }
         }
         return getCosineSimilarity(serviceContext1, serviceContext2);
     }
 
     private double intentSimilarity(String service1, String service2) {
-        return getCosineSimilarity(serviceMap.get(service1).getIntent(), serviceMap.get(service2).getIntent());
+//        return getCosineSimilarity(serviceMap.get(service1).getIntent(), serviceMap.get(service2).getIntent());
+        return 1;
     }
 
     public void addAllReverseEdges() {
         allReverseEdges = new ArrayList<Pair<String, String>>();
-        Set<DefaultWeightedEdge> edges = graph.edgeSet();
+        Set<DefaultWeightedEdge> edges = weightedGraph.edgeSet();
         for(DefaultWeightedEdge edge: edges){
-            allReverseEdges.add(new Pair<String, String>(graph.getEdgeTarget(edge), graph.getEdgeSource(edge)));
+            allReverseEdges.add(new Pair<String, String>(weightedGraph.getEdgeTarget(edge), weightedGraph.getEdgeSource(edge)));
         }
         for(Pair<String, String> reverseEdge: allReverseEdges){
-            graph.addEdge(reverseEdge.getFirst(), reverseEdge.getSecond());
+            weightedGraph.addEdge(reverseEdge.getFirst(), reverseEdge.getSecond());
         }
     }
 }
--- src/main/java/Evaluation/EvaluateDataCentricRecommendation.java
+++ src/main/java/Evaluation/EvaluateDataCentricRecommendation.java
@@ -3,6 +3,8 @@ package Evaluation;
 import DataCentricRecommendation.DataCentricRecommendation;
 import org.apache.commons.math3.util.Pair;
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
@@ -42,6 +44,8 @@ public class EvaluateDataCentricRecommendation {
         newNodes = completeGraph.vertexSet().size();
         initialEdges = inclompleteGraph.edgeSet().size();
         newEdges = completeGraph.edgeSet().size();
+        initialServicesIncidentMatrix = new HashMap<Pair<String, String>, Double>();
+        newServicesIncidentMatrix = new HashMap<Pair<String, String>, Double>();
 
         //QUESTION: should I also get the values for the initial graph ?
 
@@ -49,19 +53,74 @@ public class EvaluateDataCentricRecommendation {
         DataCentricRecommendation recommendationAlgorithm = new DataCentricRecommendation(completeGraph, allWorkflowVersions, serviceMap);
         recommendationAlgorithm.addAllReverseEdges();
 
-        for (SService service1 : serviceListWithLocals) {
-            for (SService service2 : serviceListWithLocals) {
+        for (SService service1 : serviceList) {
+            for (SService service2 : serviceList) {
                 if (!service1.equals(service2)) {
-                    double score = recommendationAlgorithm.getScore(service1, service2);
-                    newServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), score);
+                    if(inclompleteGraph.containsVertex(service1.getURL()) && inclompleteGraph.containsVertex(service2.getURL())) {
+                        DijkstraShortestPath<String, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<String, DefaultEdge>(incompleteSimpleGraph);
+                        GraphPath<String, DefaultEdge> path = dijkstraAlg.getPaths(service1.getURL()).getPath(service2.getURL());
+                        double score = 0;
+                        if(path!=null){
+                            score = 1;
+                        }
+//                        double score = recommendationAlgorithm.getScore(service1, service2);
+                        initialServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), score);
+                    }else{
+                        initialServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), -1.);
+                    }
                 }
             }
         }
 
-        for (Pair<String, String> servicePair : newServicesIncidentMatrix.keySet()) {
-            if (newServicesIncidentMatrix.get(servicePair) > 0)
-                newConnectedNodes++;
+        for (SService service1 : serviceListWithLocals) {
+            for (SService service2 : serviceListWithLocals) {
+                if (!service1.equals(service2)) {
+                    if(completeGraph.containsVertex(service1.getURL()) && completeGraph.containsVertex(service2.getURL())) {
+                        DijkstraShortestPath<String, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<String, DefaultEdge>(completeSimpleGraph);
+                        GraphPath<String, DefaultEdge> path = dijkstraAlg.getPaths(service1.getURL()).getPath(service2.getURL());
+                        double score = 0;
+                        if(path!=null){
+                            score = 1;
+                        }
+//                        double score = recommendationAlgorithm.getScore(service1, service2);
+                        newServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), score);
+                    }else{
+                        newServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), -1.);
+                    }
+                }
+            }
         }
+
+
+        for (SService service1 : serviceList) {
+            for (SService service2 : serviceList) {
+                if (!service1.equals(service2)) {
+                    Pair<String, String> pair = new Pair<String, String>(service1.getURL(), service2.getURL());
+                    if(initialServicesIncidentMatrix.containsKey(pair)){
+                        if(initialServicesIncidentMatrix.get(pair)<=0 && newServicesIncidentMatrix.get(pair)>0){
+                            newConnectedNodes++;
+                        }
+                    }
+//                    if(inclompleteGraph.containsVertex(service1.getURL()) && inclompleteGraph.containsVertex(service2.getURL())) {
+//                        DijkstraShortestPath<String, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<String, DefaultEdge>(incompleteSimpleGraph);
+//                        GraphPath<String, DefaultEdge> path = dijkstraAlg.getPaths(service1.getURL()).getPath(service2.getURL());
+//                        double score = 0;
+//                        if(path!=null){
+//                            score = 1;
+//                        }
+////                        double score = recommendationAlgorithm.getScore(service1, service2);
+//                        initialServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), score);
+//                    }else{
+//                        initialServicesIncidentMatrix.put(new Pair<String, String>(service1.getURL(), service2.getURL()), -1.);
+//                    }
+                }
+            }
+        }
+
+//        for (Pair<String, String> servicePair : newServicesIncidentMatrix.keySet()) {
+//            if (newServicesIncidentMatrix.get(servicePair) > 0)
+//                newConnectedNodes++;
+//        }
 
         print("Initial Number of Nodes: " + initialNodes);
         print("Number of Nodes with Local Workers: " + newNodes);
@@ -158,8 +217,29 @@ public class EvaluateDataCentricRecommendation {
             e.printStackTrace();
         }
 
+
+        for(WorkflowVersion workflowVersion: allWorkflowVersions){
+            workflowVersion.setIntent();
+        }
+
+
         initialServicesIncidentMatrix = new HashMap<Pair<String, String>, Double>();
         newServicesIncidentMatrix = new HashMap<Pair<String, String>, Double>();
+    }
+
+    private static void setServiceIntents(Set<SService>[] servicesTest1, Set<OOperation>[] operationsTest1) {
+        for(int i=0; i<servicesTest1.length; i++){
+            ArrayList<SService> serviceList = new ArrayList<SService>(servicesTest1[i]);
+            for(SService serivce: serviceList){
+                String intent = "";
+                for(OOperation operation: operationsTest1[i]){
+                    if (operation.getService().equals(serivce)) {
+                        intent += operation.getProcessorName() + " " + operation.getName() + " ";
+                    }
+                }
+                serivce.setIntent(intent);
+            }
+        }
     }
 
     private static Graph<String, DefaultWeightedEdge> createServiceServiceGraph(Graph<String, DefaultEdge> graph, Set<SService> services, Set<WorkflowVersion> workflowVersions) {
